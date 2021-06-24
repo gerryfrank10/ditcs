883f4fa0fb92655546036ddb5f760ca5b1d7fe77
kento <kento@localhost.localdomain>
2021-06-24 23:22:30 +0300

    App part 1 with plt working great

--- traffic/urls.py
+++ traffic/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('camera', views.camera, name='camera'),
+    path('profile', views.profile, name='profile'),
 ]
