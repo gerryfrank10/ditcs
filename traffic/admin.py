7b19e060c36c11d59a0016308e74510377f27693
medusa <gerryfrank720@gmail.com>
2021-07-05 12:36:17 +0300

    Added django report modelu

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Traffic, Light, Road
+from .reports import MyReport
 
 @admin.register(Traffic)
 class TrafficAdmin(admin.ModelAdmin):
@@ -17,4 +18,5 @@ class RoadAdmin(admin.ModelAdmin):
     list_display = ('name', 'distance', 'status')
     list_filter = ('distance','status')
 
-    
+
+#reports.register(Road, MyReport)
